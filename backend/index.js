31301f0ccd103d5874ea3e0df997b987e620b866
Sébastien Girard <sebastiengrd@gmail.com>
2022-03-14 12:33:51 -0400

    Linked backend api layer and database together Added some sample query Not dealing with possible sql injection attacks yet

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,31 @@
 const { Pool, Client } = require('pg')
-    // https://node-postgres.com/api/client
+const express = require('express')
+const app = express()
+const port = 3000
 
-const pool = new Pool({
-    user: 'username',
-    host: 'localhost',
-    database: 'default_database',
-    password: 'password',
-    port: 5432,
-})
-pool.query('SELECT NOW()', (err, res) => {
-    //console.log(err, res)
-    pool.end()
-})
 const client = new Client({
     user: 'username',
-    host: 'localhost',
+    host: 'database',
     database: 'default_database',
     password: 'password',
     port: 5432,
 })
-client.connect()
 
+app.get('/student', (req, httpRes) => {
+    client.query('SELECT * FROM student;', (err, res) => {
+        console.log(res["rows"]);
+        httpRes.send(res["rows"])
+    })
+})
 
-client.query('SELECT * FROM student;', (err, res) => {
-    console.log(res["rows"][0])
-    client.end()
+app.get('/student/:id', (req, httpRes) => {
+    client.query(`SELECT * FROM student WHERE student_id = ${req.params.id};`, (err, res) => {
+        console.log(res["rows"]);
+        httpRes.send(res["rows"])
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    client.connect();
 })
--- docker-entrypoint-initdb.d/initStudent.sql
+++ docker-entrypoint-initdb.d/initStudent.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE student
 (
 student_id INT,
