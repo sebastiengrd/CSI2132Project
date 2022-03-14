29a0f475e016aa7bf1a9d57993c275e400332c66
Sébastien Girard <sebastiengrd@gmail.com>
2022-03-14 00:32:00 -0400

    added database ddl initialisation for docker

---
+++ docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,13 @@
+
+
+CREATE TABLE student
+(
+student_id INT,
+student_name VARCHAR(30),
+student_age INT,
+student_address VARCHAR(25),
+student_phone_no VARCHAR(20)
+);
+
+INSERT INTO student values(1345674, 'student1', 135, '4 aef street', 123456789);
+INSERT INTO student values(13456754, 'student2', 125, '4 aef streee', 456123789);
