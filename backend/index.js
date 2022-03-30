44973f99beb1db06c49d8d7d35cb17611b1212b5
Sébastien Girard <sebastiengrd@gmail.com>
2022-03-30 18:55:58 -0400

    UNTESTED: added queries for our usecase
    Co-authored-by: Loic <kandloic@users.noreply.github.com>
    Co-authored-by: Hened <henedsaade@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,60 @@ app.post('/person/', (req, httpRes) => {
     client.query(`INSERT INTO Person VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`, [payload.ssn, payload.username, payload.firstName, payload.middleName, payload.lastName, payload.gender, payload.dob, payload.email, payload.phoneNumber], (err, res) => { handleBasicQueryResponse(httpRes, err, res) })
 })
 
+/* 
+    select all the appointments of a person (seb)
+*/
+app.get('/person/:patientid/appointments', (req, httpRes) => {
+    client.query(`SELECT appointid, patientid, Employee.employeeid, date, starttime, endtime, appointtype, status, room, invoiceid, emprole, branchid, firstname, middlename, lastname, phonenumber FROM (Appointment JOIN Employee on Appointment.employeeid = Employee.employeeid) JOIN Person on employee.ssn = Person.ssn WHERE patientid = $1;`, [req.params.patientid], (err, res) => { handleBasicQueryResponse(httpRes, err, res) })
+})
+
+/* 
+    select all the appointments of a doctor (seb)
+*/
+app.get('/doctor/:employeeid/appointments', (req, httpRes) => {
+    client.query(`SELECT appointid, Appointment.patientid,  date, starttime, endtime, appointtype, status, room, invoiceid, balance, firstname, middlename, lastname, gender, dateofbirth, email, phonenumber FROM (Appointment JOIN Patient on Appointment.patientId = Patient.patientId) JOIN Person on Patient.ssn = Person.ssn  WHERE employeeid = $1;`, [req.params.employeeid], (err, res) => { handleBasicQueryResponse(httpRes, err, res) })
+})
+
+/* 
+    Retrieve all procedures associated to an appointment (seb)
+*/
+app.get('/appointment/:appointmentid/procedures', (req, httpRes) => {
+    client.query(`SELECT * FROM appointmentprocedure WHERE appointmentid = $1;`, [req.params.appointmentid], (err, res) => { handleBasicQueryResponse(httpRes, err, res) })
+})
+
+
+/**
+ * Retrieving list of dentists in a branch (loic)
+ */
+app.get('/branch/:branchId/employee', (req, httpRes) => {
+    client.query(`SELECT * FROM Employee JOIN Person ON Employee.ssn = Person.ssn WHERE Employee.branchId = $1;`, [req.params.branchId], (err, res) => { handleBasicQueryResponse(httpRes, err, res) })
+})
+
+/**
+ * Add an appointment (For receptionist/admin) (loic)
+ */
+app.post('/appointments/', (req, httpRes) => {
+    payload = req.body
+    client.query(`INSERT INTO Appointment VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`, [payload.appointmentID, payload.patientID, payload.EmployeeID, payload.date, payload.startTime, payload.endTime, payload.appointmentType, payload.status, payload.room, payload.invoiceID], (err, res) => { handleBasicQueryResponse(httpRes, err, res) })
+})
+
+
+/**
+ * Retrieving all treatments associated to an appointment (hened)
+ */
+app.get('/appointment/:appointId/treatment', (req, httpRes) => {
+    client.query('SELECT * FROM Treatment WHERE appointId = $1;', [req.params.appointId], (err, res) => { handleBasicQueryResponse(httpRes, err, res) })
+})
+
+/**
+ * Add new patient (hened)
+ */
+app.post('/patient/', (req, httpRes) => {
+    payload = req.body
+    client.query('INSERT INTO Patient VALUES ($1, $2, $3);', [payload.patientId, payload.ssn, payload.balance], (err, res) => { handleBasicQueryResponse(httpRes, err, res) })
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
     client.connect();
